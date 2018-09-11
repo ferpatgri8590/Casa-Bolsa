0a193ba8c5ce93176dc4eb9b5523d23e90cfe39d
180910_GPO_SALINAS <180910_GPO_SALINAS@LAP_63>
2018-09-11 14:25:45 -0500

    Version Inicial del proyecto

---
+++ src/casadebolsa/Domicilio.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package casadebolsa;
+
+/**
+ *
+ * @author 180910_GPO_SALINAS
+ */
+public class Domicilio {
+    String calle;
+    String numero;
+    String colonia;
+    String localidad;
+    String estado;
+}
---
+++ src/casadebolsa/Movimiento.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package casadebolsa;
+
+/**
+ *
+ * @author 180910_GPO_SALINAS
+ */
+public class Movimiento {
+    
+}
