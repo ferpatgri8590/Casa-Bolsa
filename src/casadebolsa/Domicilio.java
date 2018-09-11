50ea2beb48d71f168c18b2b5277306d8e16df88d
180910_GPO_SALINAS <180910_GPO_SALINAS@LAP_63>
2018-09-11 14:49:33 -0500

    Se codifica la entidad Domicilio

--- src/casadebolsa/Domicilio.java
+++ src/casadebolsa/Domicilio.java
@@ -10,9 +10,109 @@ package casadebolsa;
  * @author 180910_GPO_SALINAS
  */
 public class Domicilio {
-    String calle;
-    String numero;
-    String colonia;
-    String localidad;
-    String estado;
+
+    private String calle;
+    private String numero;
+    private String colonia;
+    private String localidad;
+    private String estado;
+    private int codigoPostal;
+
+    public Domicilio(String calle, String numero, String colonia, String localidad, String estado, int codigoPostal) {
+        this.calle = calle;
+        this.numero = numero;
+        this.colonia = colonia;
+        this.localidad = localidad;
+        this.estado = estado;
+        this.codigoPostal = codigoPostal;
+    }
+    
+    /**
+     * @return the calle
+     */
+    public String getCalle() {
+        return calle;
+    }
+
+    /**
+     * @param calle the calle to set
+     */
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    /**
+     * @return the numero
+     */
+    public String getNumero() {
+        return numero;
+    }
+
+    /**
+     * @param numero the numero to set
+     */
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    /**
+     * @return the colonia
+     */
+    public String getColonia() {
+        return colonia;
+    }
+
+    /**
+     * @param colonia the colonia to set
+     */
+    public void setColonia(String colonia) {
+        this.colonia = colonia;
+    }
+
+    /**
+     * @return the localidad
+     */
+    public String getLocalidad() {
+        return localidad;
+    }
+
+    /**
+     * @param localidad the localidad to set
+     */
+    public void setLocalidad(String localidad) {
+        this.localidad = localidad;
+    }
+
+    /**
+     * @return the estado
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    /**
+     * @return the codigoPostal
+     */
+    public int getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    /**
+     * @param codigoPostal the codigoPostal to set
+     */
+    public void setCodigoPostal(int codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+    
+     @Override
+    public String toString() {
+        return "Domicilio{" + "calle=" + calle + ", numero=" + numero + ", colonia=" + colonia + ", localidad=" + localidad + ", estado=" + estado + ", codigoPostal=" + codigoPostal + '}';
+    }
 }
